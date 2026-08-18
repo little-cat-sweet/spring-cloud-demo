9be03fe066d5fa59330a4b9ab2b0bb03523ae5d2
Mark <wye87444@gmail.com>
2026-08-18 20:34:06 +0800

    complete sentinel. 01

--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -3,11 +3,12 @@ package com.example.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Import;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 public class UserServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
     }
--- user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.example.userservice.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,10 +14,24 @@ public class UserController {
     @Value("${server.port}")
     private String port;
     @GetMapping("/{id}")
+    @SentinelResource(
+            value = "getUser",
+            fallback = "fallback"
+    )
     public String getUser(
             @PathVariable("id") Long id
     ) {
 
+        if(id == 1){
+            throw new RuntimeException();
+        }
+
+
         return "post is " + port + ", id is " + id;
+    }
+
+    public String fallback(Long id, Throwable e){
+
+        return "用户服务暂时不可用";
     }
 }
